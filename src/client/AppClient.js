75424a86667157e2a4432c092142286f5e9aee35
mebird <mebird@users.noreply.github.com>
2021-01-09 13:31:07 -0800

    init app client

---
+++ src/client/AppClient.js
@@ -0,0 +1,48 @@
+import SwipeAction from "./SwipeAction";
+
+class AppClient {
+    /**
+     * Given project data, creates a project in the DB and returns the project ID.
+     * @param {*} projectData 
+     */
+    async createProject(projectData) {
+        return "";
+    }
+
+    /**
+     * Returns an array of all projects the authenticated user is a part of.
+     */
+    async viewProjects() {
+        return [];
+    }
+
+    /**
+     * Returns the next project the user should view.
+     */
+    async getNextProject() {
+        return {};
+    }
+
+    /**
+     * Handles a swipe on the given project
+     * @param {*} projectID 
+     * @param {string} direction Taken from the SwipeAction object
+     */
+    async swipeProject(projectID, direction) {
+        switch (direction) {
+            case SwipeAction.LEFT:
+            case SwipeAction.RIGHT:
+            default:
+        }
+    }
+
+    /**
+     * Given a user ID, returns all information associated with that user
+     * @param {number} userID 
+     */
+    async viewUser(userID) {
+        return {};
+    }
+}
+
+export default AppClient;
