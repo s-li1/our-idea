2cb815c2ea6a26b201d0c00a18c736e099afd197
mebird <mebird@users.noreply.github.com>
2021-01-09 15:17:05 -0800

    adding max note

--- src/types/Project.js
+++ src/types/Project.js
@@ -3,4 +3,8 @@
  * @typedef {Object} Project
  * @property {string} projectID
  * @property {string} name
+ * @property {string} description
+ * 
+ * // Are we still doing this in MVP?
+ * @property {number} maxMembers
  */
