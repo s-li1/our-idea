bf539f1e2459dca6a31907772125bf09e079acf2
shlyyzy <shirleyy0103@gmail.com>
2021-01-10 07:15:09 -0700

    Changed Message userID retrieval method

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -7,9 +7,9 @@ import { SessionContext } from '../../components/Session';
 function Message(props) {
   console.log(props);
   const { message: { text, senderID } } = props;
-  const { session: { userID } } = useContext(SessionContext);
+  const id = useContext(SessionContext);
   
-  const isSender = senderID == userID ? 'sent' : 'received';
+  const isSender = senderID == id.session.userID ? 'sent' : 'received';
 
   return (
     <div className={`message ${isSender}`}>
