212aa74254cee0ef664ca3f6e6d36dfe9ab7a6dc
Steven.J.Li <14027496@student.uts.edu.au>
2021-01-11 05:51:45 +1100

    new logo and name

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { FirebaseContext } from './components/Firebase';
 import ChatAppClient from './clients/ChatAppClient/ChatAppClient';
-import "react-tiger-transition/styles/main.min.css";
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
