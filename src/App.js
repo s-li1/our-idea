c03d22ff6122e8ddb869e9cc0d03bf28d6a1a182
mebird <mebird@users.noreply.github.com>
2021-01-09 21:50:14 -0800

    adding routing and hooks for chat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 
 import LandingPage from './pages/Landing/LandingPage';
@@ -7,16 +7,23 @@ import CreateAccountPage from './pages/CreateAccount/CreateAccountPage';
 import CreateProjectPage from './pages/CreateProject/CreateProjectPage';
 import LoginPage from './pages/Login/LoginPage';
 import HomePage from './pages/Home/HomePage';
+import ChatPage from './pages/Chat/ChatPage';
+import { useContext } from 'react';
+import { FirebaseContext } from './components/Firebase';
 
 function App() {
+  const client = useContext(FirebaseContext);
+
   return (
     <Router>
       <Switch>
         <Route exact path={ROUTES.LANDING} component={LandingPage}/>
         <Route path={ROUTES.CREATE_ACCOUNT} component={CreateAccountPage}/>
-        <Route path={ROUTES.CREATE_PROJECT} component={CreateProjectPage}/>
+        <Route path={ROUTES.PROJECT_CREATE} component={CreateProjectPage}/>
+        <Route path={ROUTES.PROJECT_CHAT} component={ChatPage}/>
         <Route path={ROUTES.LOGIN} component={LoginPage}/>
         <Route path={ROUTES.HOME} component={HomePage}/>
+        <Redirect to={client.auth.currentUser ? ROUTES.HOME : ROUTES.LOGIN}/>
       </Switch>
     </Router>
   );
