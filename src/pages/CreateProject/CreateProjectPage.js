7a917b818e166feb2f28e6bd9e52aa0e77646bca
Shirley Yang <shirleyy0103@gmail.com>
2021-01-10 00:51:34 -0700

    Implemented the create project page and functionality (#11)

--- src/pages/CreateProject/CreateProjectPage.js
+++ src/pages/CreateProject/CreateProjectPage.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import { React, useState } from 'react'
 import Navigation from '../../components/Navigation/Navigation'
 import Navbar from '../../components/Navbar/Navbar'
+import CreateProjectForm from './CreateProjectForm'
+import './CreateProject.css'
+import AppClient from '../../clients/BasicAppClient/BasicAppClient'
+import { FirebaseContext } from '../../components/Firebase';
 
 export default function CreateProjectPage() {
-    
     return (
-        <div>
+        <div className="screen-container">
             <Navbar />
-            <h1>Create Project</h1>
- 
+            <div className="create-project-header">
+                <h1>Create Project</h1>
+                <p>Submit your project and gauge interest!</p>
+            </div>
+            <FirebaseContext.Consumer>
+                {BasicAppClient => <CreateProjectForm appClient = {BasicAppClient}/>}
+            </FirebaseContext.Consumer>
         </div>
     )
 }
