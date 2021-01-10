f8d472dc679d2a764d36a90da612f55141f4f206
shlyyzy <shirleyy0103@gmail.com>
2021-01-10 07:32:22 -0700

    Fixed Message userID issues

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -4,12 +4,14 @@ import { FirebaseContext } from '../../components/Firebase';
 import {ChatAppClient} from '../../clients/ChatAppClient/ChatAppClient';
 import { SessionContext } from '../../components/Session';
 
+
 function Message(props) {
-  console.log(props);
-  const { message: { text, senderID } } = props;
-  const id = useContext(SessionContext);
   
-  const isSender = senderID == id.session.userID ? 'sent' : 'received';
+  const { message: { text, senderID } } = props;
+  const client = useContext(FirebaseContext);
+  const userID = client.auth.currentUser.uid;
+  console.log(userID);
+  const isSender = senderID === userID ? 'sent' : 'received';
 
   return (
     <div className={`message ${isSender}`}>
