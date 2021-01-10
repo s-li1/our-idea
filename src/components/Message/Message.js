ef1c5bf6a13af86d51e6631d5a8c732106377cae
shlyyzy <shirleyy0103@gmail.com>
2021-01-10 06:28:42 -0700

    Implemented chat feature

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,27 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import './Message.css';
+import { FirebaseContext } from '../../components/Firebase';
+import {ChatAppClient} from '../../clients/ChatAppClient/ChatAppClient';
+import { SessionContext } from '../../components/Session';
 
-function Message ({ message: { text, user }, name }) {
-  let isSentByCurrentUser = name === user ? true : false;
-  //let isSentByCurrentUser = false;
+function Message(props) {
+  console.log(props);
+  const { message: { text, senderID } } = props;
+  const { session: { userID } } = useContext(SessionContext);
+  
+  const isSender = senderID == userID ? 'sent' : 'received';
+
   return (
-    isSentByCurrentUser
-      ? (
-        <div className="messageContainer justifyEnd">
-          <div className="messageBox backgroundSent">
-            <p className="messageText">{text}</p>
-          </div>
-          <p className="sentText pr-10">{name}</p>
-        </div>
-        )
-        : (
-          <div className="messageContainer justifyStart">
-            <div className="messageBox backgroundReceived">
-              <p className="messageText">{text}</p>
-            </div>
-            <p className="sentText pl-10">{user.name}</p>
-          </div>
-        )
+    <div className={`message ${isSender}`}>
+      <p>{text}</p>
+    </div>
   );
 }
 
